8c87b5a6ddeb903d51d1f1237a6c4ce439b17f21
Kavya <kavya01092003@gmail.com>
2025-07-01 14:59:12 +0530

    all videos are showing on homepage

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': 'http://localhost:8000'
